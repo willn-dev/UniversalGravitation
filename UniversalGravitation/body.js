cdca1c0e84718da20e3bbe57b7148a1041243391
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-08-11 19:41:04 -0500

    Complete basic UG example

--- UniversalGravitation/body.js
+++ UniversalGravitation/body.js
@@ -1,12 +1,13 @@
 class Body{
 
     /**
-     @param {number} m - Mass 
+     @param {Number} vx -Velocity X
+     @param {Number} vy -Velocity Y
+     @param {number} m - Mass
      */
-    constructor(x,y,m){
+    constructor(x, y, vx, vy, m){
         this.pos = createVector(x,y);
-        this.vel = p5.Vector.random2D();
-        this.vel.mult(7)
+        this.vel = createVector(vx,vy);
         this.mass = m;
         this.accl = createVector(0,0);
         this.radius = sqrt(this.mass) * 2;
@@ -16,6 +17,19 @@ class Body{
         let f = p5.Vector.div(force, this.mass);
         this.accl.add(f);
     }
+
+    attract(body){
+      let force = p5.Vector.sub(this.pos, body.pos);
+      let distanceSq = constrain(force.magSq(),100, 1000);
+
+      let G = .5;
+      let attrMag = G * (this.mass * body.mass) / distanceSq;
+
+      force.setMag(attrMag);
+      body.applyForce(force);
+    }
+
+
 
     update(){
         this.vel.add(this.accl);
